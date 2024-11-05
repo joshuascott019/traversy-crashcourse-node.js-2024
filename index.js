dd791fa1ef0bb6fdf9c73b02751445089956ceaa
Joshua Scott <joshua.ds.s019@gmail.com>
2024-11-05 16:45:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import getPosts, { getPostsLength } from './postController.js';
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
+
+// const { rNG, celciusToFahrenheit } = require('./utils');
+
+// console.log(`Random Number: ${rNG()}`);
+
+// console.log(`Celcius to F: ${celciusToFahrenheit(rNG())}F`);
---
+++ utils.js
@@ -0,0 +1,9 @@
+function rNG() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function celciusToFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
+
+module.exports = { rNG, celciusToFahrenheit };
